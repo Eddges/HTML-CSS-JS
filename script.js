5fa8b3545e939405307fa0fe333c3150b32fd867
Eddges <shivamshekhar.i7@gmail.com>
2021-02-05 23:33:58 +0530

    JS 1

---
+++ script.js
@@ -0,0 +1,18 @@
+var a = 11
+var b = '3'
+
+var c = false
+
+var x = null
+
+
+console.log(typeof c)
+
+// data types: 
+//     -Number
+//     -String
+//     -Boolean
+//     -NaN
+//     -undefined
+//     -null
+
